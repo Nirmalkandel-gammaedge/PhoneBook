80afdbeaf281f999664c163ccb7b4c4be6f292b3
nirmal <nirmal.kandel@gammaedge.io>
2025-03-17 17:05:52 +0530

    clean

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,38 +1,37 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 export default function Profile() {
   return (
-    <Card style={{ maxWidth: 500,  display:"flex"}}>
-    
+    <Card style={{ maxWidth: 500, display: "flex" }}>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           name
         </Typography>
-         <Typography gutterBottom variant="h6" component="div">
+        <Typography gutterBottom variant="h6" component="div">
           89r30034234
         </Typography>
         <Typography gutterBottom variant="h6" component="div">
           label
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-     Avatarl <link rel="stylesheet" href="" />
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          Avatarl <link rel="stylesheet" href="" />
         </Typography>
       </CardContent>
       <CardActions>
-      <Button variant="contained" color="success">
-        Edit
-      </Button>
-      <Button variant="outlined" color="primary">
-        Update
-      </Button> <Button variant="outlined" color="error">
-       Delete
-      </Button>
+        <Button variant="contained" color="success">
+          Edit
+        </Button>
+        <Button variant="outlined" color="primary">
+          Update
+        </Button>{" "}
+        <Button variant="outlined" color="error">
+          Delete
+        </Button>
       </CardActions>
     </Card>
   );
